0c46c009875b8c49b2e43a7cdaafcefc5e4496af
Raul Rusu <raulrusu46@gmail.com>
2021-07-14 06:29:06 +0300

    Toolbar Menus

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.tutorial
 
 import android.Manifest
-import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,24 +19,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        Implicit intents -> An action that gives the user the choice to open an action with a different application
-//        eg: Making a photo (you give the user the possibility and they choose which application they make the photos
-//        eg: Share button -> the user chooses the application to share the current post
-
-        btnTakePhoto.setOnClickListener {
-            Intent(Intent.ACTION_GET_CONTENT).also {
-                it.type = "image/*"  // we can specify type of files image/png , image/jpg, with * we take everything
-                startActivityForResult(it, 0)
-            }
-        }
     }
 
-//    To receive that activity, we need to override the onActivityResult function and check if there's data
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK && requestCode == 0)  {
-            val uri = data?.data
-            ivPhoto.setImageURI(uri)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miAddContact -> Toast.makeText(this, "You clikced on Add Contact", Toast.LENGTH_SHORT).show()
+            R.id.miFavorites -> Toast.makeText(this, "You clikced on Favorites", Toast.LENGTH_SHORT).show()
+            R.id.miSettings -> Toast.makeText(this, "You clikced on Settings", Toast.LENGTH_SHORT).show()
+            R.id.miFeedback -> Toast.makeText(this, "You clikced on Feedback", Toast.LENGTH_SHORT).show()
+            R.id.miClose -> finish()
         }
+        return true
     }
 }
